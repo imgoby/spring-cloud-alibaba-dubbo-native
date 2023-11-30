17f50b3d727cf3a36ba37866741d5d82abc18a5f
yuhoutian <yuhoutian@y.run>
2023-11-30 23:21:36 +0800

    Despite circular references being allowed, the dependency cycle between beans could not be broken. Update your application to remove the dependency cycle

--- spring-cloud-alibaba-starter-dubbo/src/main/java/com/alibaba/cloud/dubbo/service/DubboMetadataServiceExporter.java
+++ spring-cloud-alibaba-starter-dubbo/src/main/java/com/alibaba/cloud/dubbo/service/DubboMetadataServiceExporter.java
@@ -51,7 +51,7 @@ public class DubboMetadataServiceExporter {
 	private ObjectProvider<DubboMetadataService> dubboMetadataService;
 
 	//resolve circular references.yuhou.todo
-//	@Lazy
+	@Lazy
 	@Autowired
 	private Supplier<ProtocolConfig> protocolConfigSupplier;
 
