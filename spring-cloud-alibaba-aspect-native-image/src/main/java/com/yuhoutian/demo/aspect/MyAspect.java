b94d4ac9fab9bb496ec1f3760fa39f3623c20720
yuhoutian <yuhoutian@y.run>
2023-12-02 16:25:18 +0800

    add aspect

--- spring-cloud-alibaba-aspect-native-image/src/main/java/com/yuhoutian/demo/aspect/MyAspect.java
+++ spring-cloud-alibaba-aspect-native-image/src/main/java/com/yuhoutian/demo/aspect/MyAspect.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 @Aspect  // 使用@Aspect注解声明一个切面
 @Component
 public class MyAspect {
-    @Before(value = "execution(* com.space.aspect.Task.exec(..))")
+    @Before(value = "execution(* com.yuhoutian.demo.aspect.Task.exec(..))")
     public void before() throws Throwable {
         System.out.println("前置打印：----------------------------------------");
     }
