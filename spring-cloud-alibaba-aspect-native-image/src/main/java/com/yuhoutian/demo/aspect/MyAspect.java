6136c398428c39cdafcae87cb9de43e4c71f4b99
yuhoutian <yuhoutian@y.run>
2023-12-02 16:21:34 +0800

    add aspect

--- spring-cloud-alibaba-aspect-native-image/src/main/java/com/yuhoutian/demo/aspect/MyAspect.java
+++ spring-cloud-alibaba-aspect-native-image/src/main/java/com/yuhoutian/demo/aspect/MyAspect.java
@@ -4,11 +4,10 @@ import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
 
-
-@Aspect
+@Aspect  // 使用@Aspect注解声明一个切面
 @Component
 public class MyAspect {
-    @Before(value = "execution(* com.yuhoutian.demo.aspect.Task.exec(..))")
+    @Before(value = "execution(* com.space.aspect.Task.exec(..))")
     public void before() throws Throwable {
         System.out.println("前置打印：----------------------------------------");
     }
--- spring-cloud-alibaba-aspect-native-image/src/main/java/com/yuhoutian/demo/aspect/TestController.java
+++ spring-cloud-alibaba-aspect-native-image/src/main/java/com/yuhoutian/demo/aspect/TestController.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
 @RestController
 public class TestController {
 
