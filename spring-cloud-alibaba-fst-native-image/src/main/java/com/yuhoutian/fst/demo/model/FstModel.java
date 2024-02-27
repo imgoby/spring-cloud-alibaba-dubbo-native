66aee88fd0c42d9909b4510064f50e1035ee3e7e
goby <imgoby@163.com>
2024-02-27 10:18:12 +0800

    增加fst native实例

--- spring-cloud-alibaba-fst-native-image/src/main/java/com/yuhoutian/fst/demo/model/FstModel.java
+++ spring-cloud-alibaba-fst-native-image/src/main/java/com/yuhoutian/fst/demo/model/FstModel.java
@@ -2,8 +2,10 @@ package com.yuhoutian.fst.demo.model;
 
 import lombok.Data;
 
+import java.io.Serializable;
+
 @Data
-public class FstModel {
+public class FstModel implements Serializable {
     private long id;
     private String name;
 }
