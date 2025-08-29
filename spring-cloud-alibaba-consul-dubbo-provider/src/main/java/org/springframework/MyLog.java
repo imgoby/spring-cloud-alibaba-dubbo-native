1246a74bacd740d16155570b6cfbfc1a65c2df96
imgoby <imgoby@163.com>
2025-08-29 09:13:26 +0800

    追踪调用

---
+++ spring-cloud-alibaba-consul-dubbo-provider/src/main/java/org/springframework/MyLog.java
@@ -0,0 +1,41 @@
+package org.springframework;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
+public class MyLog {
+    private static MyLog instance=new MyLog();
+    private FileOutputStream fos;
+
+    private MyLog(){
+        try {
+            fos = new FileOutputStream("/tmp/log.log", true);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static MyLog getInstance(){
+        return instance;
+    }
+
+    public synchronized void write(String text){
+        try{
+            fos.write(text.getBytes());
+            fos.flush();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public synchronized void write(byte[] bs){
+        try{
+            fos.write(bs);
+            fos.flush();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+
+}
